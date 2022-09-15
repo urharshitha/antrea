a206094cd83eed3a53f3700b11b7beefb3d7da87
Harshitha U R <64474108+urharshitha@users.noreply.github.com>
2022-09-15 15:03:03 +0530

    Update transform.go

--- pkg/antctl/transform/addressgroup/transform.go
+++ pkg/antctl/transform/addressgroup/transform.go
@@ -14,11 +14,10 @@
 
 package addressgroup
 
-import (
-	"io"
-	"reflect"
-
-	"antrea.io/antrea/pkg/antctl/transform"
+import ( //"io"
+	//"reflect"
+	//"sort"
+	//"antrea.io/antrea/pkg/antctl/transform"
 	"antrea.io/antrea/pkg/antctl/transform/common"
 	cpv1beta "antrea.io/antrea/pkg/apis/controlplane/v1beta2"
 )
@@ -29,39 +28,9 @@ type Response struct {
 	Nodes []common.GroupMember `json:"nodes,omitempty"`
 }
 
-func listTransform(l interface{}, opts map[string]string) (interface{}, error) {
-	groups := l.(*cpv1beta.AddressGroupList)
-	result := []interface{}{}
-	for i := range groups.Items {
-		item := groups.Items[i]
-		o, _ := objectTransform(&item, opts)
-		result = append(result, o.(Response))
-	}
-	return result, nil
-}
-
-func objectTransform(o interface{}, _ map[string]string) (interface{}, error) {
-	group := o.(*cpv1beta.AddressGroup)
-	var pods, nodes []common.GroupMember
-	for _, member := range group.GroupMembers {
-		gm := common.GroupMemberTransform(member)
-		if member.Node != nil {
-			nodes = append(nodes, gm)
-			continue
-		}
-		pods = append(pods, gm)
-	}
-	return Response{Name: group.Name, Pods: pods, Nodes: nodes}, nil
-}
-
-func Transform(reader io.Reader, single bool, opts map[string]string) (interface{}, error) {
-	return transform.GenericFactory(
-		reflect.TypeOf(cpv1beta.AddressGroup{}),
-		reflect.TypeOf(cpv1beta.AddressGroupList{}),
-		objectTransform,
-		listTransform,
-		opts,
-	)(reader, single)
+type Adsorter struct {
+	Addressgroups []cpv1beta.AddressGroup
+	SortBy        string
 }
 
 var _ common.TableOutput = new(Response)
